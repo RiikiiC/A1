b796aaeb61383afb6f8b950a0ed76b048a28f830
Riikii Chen <chexiyua@sheridancollege.ca>
2025-09-21 23:59:18 -0400

    Create hospital data
    Create a data structure that describes a "hospital". It has a name and contains 3 "patient" records. Each "patient" has an id, fullName, dateOfBirth, and multiple "symptoms". Each symptom in a String.

--- A1Programming.js
+++ A1Programming.js
@@ -4,9 +4,21 @@ let hospital = {
     patients:[
         {
             id:"001",
-            fullName:"",
-            dateOfBirth:"",
-            symptom:""
+            fullName:"Ted Mosby",
+            dateOfBirth:"1978.04.25",
+            symptom:["runny nose", "sore throat", "cough"]
+        },
+        {
+            id:"002",
+            fullName:"Marshall Eriksen",
+            dateOfBirth:"1978.11.02",
+            symptom:["Stomach pain", "vomiting", "burping"]
+        },
+        {
+            id:"003",
+            fullName:"Lily Aldrin",
+            dateOfBirth:"1978.03.22",
+            symptom:["Sneezing", "stuffy nose", "headache"]
         }
     ]
 }
