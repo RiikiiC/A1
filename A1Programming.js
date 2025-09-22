b1c11b315a06ef969f3fa0bf2dadc3ebc5ca5ff7
Riikii Chen <chexiyua@sheridancollege.ca>
2025-09-21 23:20:36 -0400

    Create A1 file
    Create new files

---
+++ A1Programming.js
@@ -0,0 +1,12 @@
+// 1. Create hospital data
+let hospital = {
+    name:"Oakville Public Hospital",
+    patients:[
+        {
+            id:"001",
+            fullName:"",
+            dateOfBirth:"",
+            symptom:""
+        }
+    ]
+}
