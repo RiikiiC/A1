67e53a91b32a643869f82f5dc77b916a2cf5771c
Riikii Chen <chexiyua@sheridancollege.ca>
2025-09-22 20:42:24 -0400

    Create a function named "showPatients"
    Create a function named "showPatients" that receives a hospital data structure

--- A1Programming.js
+++ A1Programming.js
@@ -21,4 +21,52 @@ let hospital = {
             symptom:["Sneezing", "stuffy nose", "headache"]
         }
     ]
+};
+
+
+// 2. Create a function named "showPatients"
+function showPatients(patientsLi){
+    let hospitalData = document.querySelector("#hospital");
+
+    let newName = document.createElement("h1")
+    newName.textContent = hospital.name;
+    hospitalData.appendChild(newName);
+    console.log(newName)
+    
+    function createPatientsInfo(patient) {
+
+        for(let i=0; i<hospital.patients.length; i++){
+            let patientsInfo = document.createElement("h2");
+            
+            patientsInfo.textContent = `${hospital.patients[i].fullName}
+                ${hospital.patients[i].dateOfBirth}`;
+
+            hospitalData.appendChild(patientsInfo);
+            console.log(patientsInfo);
+
+            function createSymptomsList(symptomsArray){
+                let symptomList = document.createElement("ul");
+                
+                for (let j = 0; j < symptomsArray.length; j++) {
+                    let newLi = document.createElement("li");
+                    newLi.textContent = symptomsArray[j];
+                    symptomList.appendChild(newLi);
+                    console.log(newLi)
+                };
+            
+                hospitalData.appendChild(symptomList)
+                return symptomList;
+            }
+            let symptom = createSymptomsList(hospital.patients[i].symptom);
+            console.log(symptom);
+        }            
+
+    }
+    let info = createPatientsInfo(patientsLi);
+    // console.log(info);
+
+    return hospitalData.innerHTML;
+
 }
+let data = showPatients(hospital);
+// console.log(data);
